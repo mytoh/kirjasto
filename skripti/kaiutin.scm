a9c510fff25a57513e4e93cc9ed315cef508e154
mytoh <mytoh@gmail.com>
2012-10-15 23:49:07 +0900

    add sounds from 2ch

--- skripti/kaiutin.scm
+++ skripti/kaiutin.scm
@@ -8,16 +8,20 @@
   (tree->string
     '("o4" "l8" "F#" "D" "<A>" "D" "E" ">A<" "P8." "<A>" "E" "F#" "E" "<A>" "D2"))
   ; fa re "la" re  mi 'la' - "la"  mi fa mi "la" re -
-    ; "o4l8F#D<A>DE>A<P8.<A>EF#E<A>D2"
   )
 
 (define (marisa)
-"T155MLL16E-FG-2.P8FG-L2A->D-<B-1PL4B->D-E-2E-G-F.D-.<A-G-.A-.B-F2L8F.G-.A-G-2.P8L16FG-A-2>D-2E-1L8PG-F4E-.<B-.>D-L4D-.<B.A->D.E-.FF.E-2"
-)
+  ; "http://toro.2ch.net/test/read.cgi/unix/1012808941"
+  "T155MLL16E-FG-2.P8FG-L2A->D-<B-1PL4B->D-E-2E-G-F.D-.<A-G-.A-.B-F2L8F.G-.A-G-2.P8L16FG-A-2>D-2E-1L8PG-F4E-.<B-.>D-L4D-.<B.A->D.E-.FF.E-2"
+  )
+
+(define (jishin)
+  ; "http://toro.2ch.net/test/read.cgi/unix/1012808941"
+  "T160MLO4L32G>CEA#>D#8<<G#>C#FB>E8<<P2G>CEA#>D#8<<G#>C#FB>E8<<P2")  
 
 
 (define (kaiutin notes)
- (call-with-output-file
+  (call-with-output-file
     "/dev/speaker"
     (lambda (in)
       (display (notes)
@@ -25,5 +29,5 @@
   )
 
 (define (main args)
-  (kaiutin marisa)
+  (kaiutin famima)
   )
