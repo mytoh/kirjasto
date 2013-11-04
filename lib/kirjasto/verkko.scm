488c4eff04577e56912776739423d66b91863a69
mytoh <mytoh@gmail.com>
2013-11-05 07:20:40 +0900

    not use verkko.palvelin

--- lib/kirjasto/verkko.scm
+++ lib/kirjasto/verkko.scm
@@ -2,11 +2,11 @@
 
 (define-module kirjasto.verkko
   (extend
-    kirjasto.verkko.nico
-    kirjasto.verkko.avata
-    kirjasto.verkko.keitto
-    kirjasto.verkko.merkintä
-    kirjasto.verkko.palvelin
-    kirjasto.verkko.scm
-    kirjasto.verkko.työkalu
-    ))
+   kirjasto.verkko.nico
+   kirjasto.verkko.avata
+   kirjasto.verkko.keitto
+   kirjasto.verkko.merkintä
+   ;; kirjasto.verkko.palvelin
+   kirjasto.verkko.scm
+   kirjasto.verkko.työkalu
+   ))
