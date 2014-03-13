fd095b8564f47d847d5fe9da0c8bcc1d3cd1c685
mytoh <mytoh@gmail.com>
2014-03-13 17:01:56 +0900

    rewrite as r7rs library

--- lib/kirjasto/config.scm
+++ lib/kirjasto/config.scm
@@ -1,27 +1,27 @@
 
+(define-library (kirjasto config)
+    (export
+      read-config
+      load-config
+      )
+  (import
+    (scheme base)
+    (gauche base)
+    (gauche parameter)
+    (file util)))
 
-(define-module kirjasto.config
-  (export
-    read-config
-    load-config
-    )
-  (use gauche.parameter)
-  (use file.util))
-(select-module kirjasto.config)
+(begin
+  (define (read-config filename :optional :key (optional? #f))
+    (if (file-exists? filename)
+      (let ((forms (file->sexp-list filename)))
+        (if-let1 error-message (cond ((null? forms) "No config data in %s")
+                                     ((< 1 (length forms)) "Too many forms in %s")
+                                     (else #f))
+                 (errorf error-message filename)
+                 (car forms)))
+      (if (not optional?)
+        (error (string-append  "Cannot find config resource " filename))
+        #f)))
 
-
-(define (read-config filename :optional :key (optional? #f))
-  (if (file-exists? filename)
-    (let ((forms (file->sexp-list filename)))
-      (if-let1 error-message (cond ((null? forms) "No config data in %s")
-                       ((< 1 (length forms)) "Too many forms in %s")
-                       (else #f))
-        (errorf error-message filename)
-        (car forms)))
-    (if (not optional?)
-      (error (string-append  "Cannot find config resource " filename))
-      #f)))
-
-(define (load-config filename . args)
-  (eval (apply read-config filename args) (interaction-environment)))
-
+  (define (load-config filename . args)
+    (eval (apply read-config filename args) (interaction-environment))))
