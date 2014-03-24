de4029f931fa5899973c9e2bf8d2c2390c7edb97
mytoh <mytoh@gmail.com>
2014-03-24 20:15:12 +0900

    * lib/kirjasto/threading.scm: rename from chicken/clojurian

---
+++ lib/kirjasto/threading.scm
@@ -0,0 +1,56 @@
+;; author "Moritz Heidkamp"
+
+(define-library (kirjasto threading)
+    (export
+      doto
+      ->
+      ->*
+      ->>
+      ->>*)
+  (import
+    (scheme base))
+
+  (begin
+    (define-syntax doto
+      (syntax-rules ()
+        ((_ x) x)
+        ((_ x (fn args ...) ...)
+         (let ((val x))
+           (fn val args ...)
+           ...
+           val))))
+
+    (define-syntax ->
+      (syntax-rules ()
+        ((_ x (f v) f2 ...)
+         (-> (f x v) f2 ...))
+        ((_ x f f2 ...)
+         (-> (f x) f2 ...))
+        ((_ x (f v))
+         (-> (f x v)))
+        ((_ x f)
+         (f x))
+        ((_ x) x)))
+
+    (define-syntax ->>
+      (syntax-rules ()
+        ((_ x (y ...) rest ...)
+         (->> (y ... x) rest ...))
+        ((_ x) x)))
+
+    (define-syntax ->*
+      (syntax-rules ()
+        ((_ x (y z ...) rest ...)
+         (->* (receive args x
+                       (apply y (append args (list z ...))))
+              rest ...))
+        ((_ x) x)))
+
+    (define-syntax ->>*
+      (syntax-rules ()
+        ((_ x) x)
+        ((_ x (y z ...) rest ...)
+         (->>* (receive args x
+                        (apply y (append (list z ...) args)))
+               rest ...))))
+    ))
