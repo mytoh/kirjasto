6b9102309aab0f776642070e83360d20133f1728
mytoh <mytoh@gmail.com>
2014-05-22 05:02:39 +0900

    * lib/kirjasto/avain.scm: add alist functions wrapper
    Now support only association list.

---
+++ lib/kirjasto/avain.scm
@@ -0,0 +1,40 @@
+;;; kv.scm
+
+(define-library (kirjasto avain)
+    (export get
+            update
+            alist?)
+  (import (scheme base)
+          (scheme write)
+          (srfi 1))
+
+  (begin
+
+    (define (get key kv)
+      (cond
+        ((alist? kv)
+         (cdr (assoc key kv)))
+        (else #false)))
+
+    (define (update key datum kv)
+      (if (alist? kv)
+        (let loop ((kv kv)
+                   (res '()))
+             (if (null? kv)
+               (reverse res)
+               (if (equal? key (car (car kv)))
+                 (loop (cdr kv)
+                       (alist-cons key datum res))
+                 (loop (cdr kv)
+                       (cons (car kv)
+                         res)))))
+        #false))
+
+    (define (alist? x)
+      (if (list? x)
+        (let ((e (car x)))
+          (and (pair? e)
+            (not (list? e))))
+        #false))
+
+    ))
